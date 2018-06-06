1abe1f80ec9acffc3da499960cb022820bf0510e
lfeng1 <lfeng@memphis.edu>
2018-06-06 15:32:43 -0500

    initial commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lianfeng/Documents/training/django_tutorial/lydiasite/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
